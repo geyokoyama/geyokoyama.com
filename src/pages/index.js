b289d5e1b15ea055833c115e947e4ba07c6479d6
geyokoyama <george_y@outlook.com>
2022-09-06 18:40:04 -0700

    Looks ugly... Need to design better

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,21 @@ const Index = () => {
     <article className="article">
       <div className="container">
         <header className="article__header">
-          <h1 className="article__header--title">Hey there, I'm George.</h1>
-          <div className="article__header--summary">
-            <p>I'm a web developer who likes gaming, reading, and playing the violin.</p>
+          <div>
+            <h1 className="article__header--title">Hey there, I'm George.</h1>
+            <div className="article__header--summary">
+              <p>I'm a web developer who likes gaming, reading, and playing the violin.</p>
+            </div>
           </div>
-          <StaticImage
-            className="article__header--image"
-            alt="Profile picture"
-            src="../assets/profile pic.png"
-          />
         </header>
         <div className="article__content">
           <section className="article__content__section">
             <h2>About</h2>
+            <StaticImage
+              className="article__header--image"
+              alt="Profile picture"
+              src="../assets/profile pic.png"
+            />
             <p>Hello there! Welcome to the website of George Yokoyama. A Japanese speaking, tutorial watching, web novel reading, PC gaming web developer currently based in Dublin, California.</p>
             <p>My motivation for this website is to have place to share what I've learned and to document my thoughts. It's built with <a href="https://www.gatsbyjs.com/">Gatsby</a>, deployed on <a href="https://www.netlify.com/">Netlify</a>, and the code for this site is available <a href="https://www.github.com/geyokoyama/geyokoyama.com">here</a>.</p>
           </section>
