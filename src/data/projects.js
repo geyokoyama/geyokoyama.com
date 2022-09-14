22898cf8c7f7a18c143c9e317d94757814716287
geyokoyama <george_y@outlook.com>
2022-09-14 16:39:45 -0700

    Update project description

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,13 +2,13 @@ const projects = [
   {
     title: "geyokoyama.com",
     slug: "geyokoyama.com",
-    description: "The repo for this website. Built with GatsbyJS and deployed on Netlify.",
+    description: "The repo for this website. Built with GatsbyJS, designed with Tailwind CSS, and deployed on Netlify.",
     repo: "https://github.com/geyokoyama/geyokoyama.com",
   },
   {
     title: "Basic Blog",
     slug: "basic-blog",
-    description: "A Ruby on Rails blog based on a tutorial.",
+    description: "A blogging application built with Ruby on Rails based on a tutorial. I deviated from the tutorial to create my own design.",
     repo: "https://github.com/geyokoyama/basic-blog",
     demo: "https://basic-blog-geyokoyama.herokuapp.com",
   },
@@ -21,7 +21,7 @@ const projects = [
   {
     title: "Message Me",
     slug: "message-me",
-    description: "A Ruby on Rails chat app based on a tutorial.",
+    description: "Another Ruby on Rails application based on a tutorial. This one is a messaging app and uses WebSockets for real-time communication.",
     repo: "https://github.com/geyokoyama/message-me",
     demo: "https://message-me-geyokoyama.herokuapp.com",
   },
